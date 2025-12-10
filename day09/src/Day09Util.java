5b25c158fb8b3d3f933cc6259fefacbe268add54
Pierre Lemerle <pierre.lemerle@gmail.com>
2025-12-10 13:55:30 +0100

    day09 part2 is solved. Most ugly code ever. Need to hard code some size limitation, to be executable in reasonable time, also need to run it with 12g memory at least since the grid takes about 10g memory to build. but all in all, it was just enough to get the puzzle solution! cant believe it! amazing

--- day09/src/Day09Util.java
+++ day09/src/Day09Util.java
@@ -20,6 +20,47 @@ public class Day09Util {
         // Return only the first 'numberOfPairs' closest pairs
         return rectangles;
     }
+
+    //part 2
+    public static List<Rectangle> findRectanglesPart2(List<Point2D> points, boolean[][] grid) {
+        List<Rectangle> rectangles = new ArrayList<>();
+        // Loop through all combinations of points and list their distances
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                Point2D pointA = points.get(i);
+                Point2D pointB = points.get(j);
+                long size = calculateSize(pointA, pointB);
+                
+                //ugly brut forcing filtering to limit execution time
+                if (size >= 904044132L && size <= 1959386940L )
+                {
+                    // add logic to verify it is fully contained in the right region prior to adding it to the list 
+                    long minX = Math.min(pointA.getX(), pointB.getX())+2;
+                    long maxX = Math.max(pointA.getX(), pointB.getX())-2;
+                    long minY = Math.min(pointA.getY(), pointB.getY())+2;
+                    long maxY = Math.max(pointA.getY(), pointB.getY())-2;
+                    boolean isValid = true;
+
+                    for (long y=minY; y<=maxY; y++){
+                        for (long x=minX; x<=maxX; x++){
+                            if (grid[(int)y][(int)x]){
+                             isValid = false;
+                                break;
+                            }   
+                        }
+                    }   
+                    if (isValid){
+                        rectangles.add(new Rectangle(pointA, pointB, size));
+                        System.out.println("added rectangle size: " + size);
+                    }
+                }
+            }
+        }
+        // Sort the pairs based on distance
+        rectangles.sort((rect1, rect2) -> Double.compare(rect1.getSize(), rect2.getSize()));
+        // Return only the first 'numberOfPairs' closest pairs
+        return rectangles;
+    }
     
     private static long calculateSize(Point2D pointA, Point2D pointB) {
         long deltaX = 0;
@@ -37,10 +78,87 @@ public class Day09Util {
         return deltaX * deltaY;
     }
 
-    public static void printGrid(char[][] grid) {
-        for (char[] row : grid) {
-            for (char tile : row) {
-                System.out.print(tile);
+    public static boolean[][] buildGrid(List<Point2D> redTiles, int width, int height) {
+        // Initialize grid with empty tiles
+        boolean[][] grid = new boolean[height][width];
+        for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    grid[i][j] = false;
+                }
+        }
+
+        // Mark the red tiles
+        for (Point2D tile : redTiles) {
+            long x = tile.getX();
+            long y = tile.getY();
+            if (x >= 0 && x < width && y >= 0 && y < height) {
+                grid[(int)y][(int)x] = true; // Mark red tile
+            }
+        }
+
+        // Connect the red tiles with green tiles
+        for (int i = 0; i < redTiles.size(); i++) {
+            Point2D current = redTiles.get(i);
+            Point2D next = redTiles.get((i + 1) % redTiles.size()); // Wrap around to the first tile
+
+            long startX = Math.min(current.getX(), next.getX());
+            long endX = Math.max(current.getX(), next.getX());
+            long startY = Math.min(current.getY(), next.getY());
+            long endY = Math.max(current.getY(), next.getY());
+
+            // Horizontal connection
+            for (long x = startX; x <= endX; x++) {
+                if (current.getY() >= 0 && current.getY() < height) {
+                    grid[(int)current.getY()][(int)x] = true; // Mark green tile
+                }
+            }
+
+            // Vertical connection
+            for (long y = startY; y <= endY; y++) {
+                if (next.getX() >= 0 && next.getX() < width) {
+                    grid[(int)y][(int)next.getX()] = true; // Mark green tile
+                }
+            }
+        }
+        return grid;
+    }
+
+    private static boolean[][] fillInternalArea(boolean[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+
+        // Fill internal area
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j]) {
+                    j++;
+                    if (grid[i][j]) {
+                        //if we are in a zone we need to skip the whole
+                        while (grid[i][j]) {
+                            j++;
+                        }
+                    }
+                    else {
+                        //otherwise we color until the end of the zone
+                        while (!grid[i][j]) {
+                            grid[i][j] = true;   
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+        return grid;
+    }
+
+    public static void printGrid(boolean[][] grid) {
+        for (boolean[] row : grid) {
+            for (boolean tile : row) {
+                if (tile) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
             }
             System.out.println();
         }
--- day09/src/Main.java
+++ day09/src/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,13 +14,18 @@ public class Main {
             
             // Find the closest pairs and then build the connections
             List<Rectangle> rectangles = Day09Util.findRectangles(points);
-            
-            long answerPart1 = rectangles.getLast().getSize();
-            //int answerPart2 = Day09Util.calculateAnswer2(closestPairs.get(closingconnection-1));
+            long answerPart1 = rectangles.get(rectangles.size() - 1).getSize();
 
-            // Output the results
             System.out.println("The answer for Part 1 is: " + answerPart1);     
-            //System.out.println("The answer for Part 2 is: " + answerPart2);
+
+            // For part 2
+            boolean[][] grid = Day09Util.buildGrid(points, 100000, 100000);
+            //Day09Util.printGrid(grid);
+            rectangles = Day09Util.findRectanglesPart2(points, grid);
+            long answerPart2 = rectangles.get(rectangles.size() - 1).getSize();
+            //Day09Util.printGrid(grid);
+            // Output the results
+            System.out.println("The answer for Part 2 is: " + answerPart2);
         } catch (IOException e) {
             System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
         }
